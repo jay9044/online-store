4c277560a92e11bf7cb589540d1f70f43140510e
jamal westfield <jamalwestfield@gmail.com>
2019-01-30 09:30:29 +0000

    using Joi for validation

---
+++ src/components/common/Form.jsx
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import Joi from "joi-browser";
+
+class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {},
+      errors: {}
+    };
+  }
+
+  validateForm() {
+    //validate the whole form
+    const { data } = this.state;
+    const options = { abortEarly: false };
+    const { error } = Joi.validate(data, this.schema, options);
+
+    const errors = {};
+    if (!error) return null;
+
+    for (let item of error.details) errors[item.path[0]] = item.message;
+    return errors;
+  }
+
+  // validate each input field
+  validateProperty({ name, value }) {
+    // destructuring the name and value of the input
+    // create a new object because using joi.validate with state data will validate entire form
+    const obj = { [name]: value }; //dynamically use name, as set its value// e.g username or password // computed propertie es6
+    const schema = { [name]: this.schema[name] };
+    const { error } = Joi.validate(obj, schema);
+    return error ? error.details[0].message : null;
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    const errors = this.validateForm();
+    this.setState({
+      errors: errors || {} // instead of returning null if there are no errors it will return an empy object
+    });
+    if (errors) return;
+
+    // call server
+    this.doSubmit();
+  }
+
+  handleChange({ currentTarget: input }) {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
+    //onchange to update data state with user input
+    const data = { ...this.state.data };
+    data[input.name] = input.value;
+    this.setState({
+      data,
+      errors
+    });
+  }
+}
+
+export default Form;
--- src/components/common/LoginForm.jsx
+++ src/components/common/LoginForm.jsx
@@ -1,69 +1,37 @@
 import React, { Component } from "react";
+import Joi from "joi-browser";
 import FormInput from "./FormInput";
+import Form from "./Form";
 
-class LoginForm extends Component {
+class LoginForm extends Form {
   constructor(props) {
     super(props);
     this.state = {
-      account: { username: "", password: "" },
+      data: { username: "", password: "" },
       errors: {}
     };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
   }
 
-  validate() {
-    const { account } = this.state;
-    const errors = {};
-    if (account.username.trim() === "") errors.username = "Username required";
+  schema = {
+    username: Joi.string()
+      .required()
+      .label("Username"),
+    password: Joi.string()
+      .required()
+      .label("Password")
+  };
 
-    if (account.password.trim() === "") errors.password = "Password required";
-
-    return Object.keys(errors).length === 0 ? null : errors;
-  }
-
-  validateProperty(input) {
-    if (input.name === "username") {
-      if (input.value.trim() === "") return "Username is required";
-    }
-    if (input.name === "password") {
-      if (input.value.trim() === "") return "Password is required";
-    }
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-
-    const errors = this.validate();
-    this.setState({
-      errors: errors || {} // instead of returning null if there are no errors it will return an empy object
-    });
-    if (errors) return;
-    // call server
+  doSubmit() {
+    //call the server
     //save changes
     //then redirect the user
-  }
-
-  handleChange({ currentTarget: input }) {
-    //validation  //this.validate will validate the entire form, I dont want that
-    const errors = { ...this.state.errors };
-    const errorMessage = this.validateProperty(input);
-
-    if (errorMessage) errors[input.name] = errorMessage;
-    else delete errors[input.name];
-
-    //onchange to update account state with user input
-    const account = { ...this.state.account };
-    account[input.name] = input.value;
-    this.setState({
-      account,
-      errors
-    });
+    console.log("submitted");
   }
 
   render() {
-    const { account, errors } = this.state;
-
+    const { data, errors } = this.state;
     return (
       <div>
         <h1>Login</h1>
@@ -71,18 +39,20 @@ class LoginForm extends Component {
           <FormInput
             name="username"
             id="username"
-            value={account.username.value}
+            value={data.username.value}
             onChange={this.handleChange}
             error={errors.username}
           />
           <FormInput
             name="password"
             id="password"
-            value={account.password.value}
+            value={data.password.value}
             onChange={this.handleChange}
             error={errors.password}
           />
-          <button className="btn btn-primary">Login</button>
+          <button disabled={this.validateForm()} className="btn btn-primary">
+            Login
+          </button>
         </form>
       </div>
     );
